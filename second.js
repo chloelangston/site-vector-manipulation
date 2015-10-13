7b17a73263400583cccbfe3bdac217ddf23dbf2e
Chloe Langston <chloealangston@gmail.com>
2015-10-13 11:01:40 -0700

    styling

--- second.js
+++ second.js
@@ -1,82 +1,65 @@
 $(function(){
 
   $('#responsive-sites').hover(function(){
-    $('.first').fadeTo("slow", 1, function(){});
     $('.resp').css("opacity", "1").addClass("right-list-anim")
     $(this).css("opacity", "0.8")
-
   }, function(){
-    $('.first').fadeTo("fast", 0, function(){})
     $('.resp').css("opacity", "0.5").removeClass("right-list-anim")
     $(this).css("opacity", "0.5")
   })
 
   $('#dash-data').hover(function(){
-    $('.second').fadeTo("slow", 1, function(){})
     $('.dash').css("opacity", "1").addClass("right-list-anim")
     $(this).css("opacity", "0.8")
   }, function(){
-    $('.second').fadeTo("fast", 0, function(){})
     $('.dash').css("opacity", "0.5").removeClass("right-list-anim")
     $(this).css("opacity", "0.5")
   })
 
   $('#content-man').hover(function(){
-    $('.third').fadeTo("slow", 1, function(){})
     $('.cont').css("opacity", "1").addClass("right-list-anim")
     $(this).css("opacity", "0.8")
   }, function(){
-    $('.third').fadeTo("fast", 0, function(){})
     $('.cont').css("opacity", "0.5").removeClass("right-list-anim")
     $(this).css("opacity", "0.5")
   })
 
   $('#art-int').hover(function(){
-    $('.fourth').fadeTo("slow", 1, function(){})
     $('.art').css("opacity", "1").addClass("right-list-anim")
     $(this).css("opacity", "0.8")
   }, function(){
-    $('.fourth').fadeTo("fast", 0, function(){})
     $('.art').css("opacity", "0.5").removeClass("right-list-anim")
     $(this).css("opacity", "0.5")
   })
 
   $('#nat-lang').hover(function(){
-    $('.fifth').fadeTo("slow", 1, function(){})
     $('.nat').css("opacity", "1").addClass("right-list-anim")
     $(this).css("opacity", "0.8")
   }, function(){
-    $('.fifth').fadeTo("fast", 0, function(){})
     $('.nat').css("opacity", "0.5").removeClass("right-list-anim")
     $(this).css("opacity", "0.5")
   })
 
   $('#web-serv').hover(function(){
-    $('.sixth').fadeTo("slow", 1, function(){})
     $('.web').css("opacity", "1").addClass("right-list-anim")
     $(this).css("opacity", "0.8")
   }, function(){
-    $('.sixth').fadeTo("fast", 0, function(){})
     $('.web').css("opacity", "0.5").removeClass("right-list-anim")
     $(this).css("opacity", "0.5")
   })
 
   $('#loc-apps').hover(function(){
-    $('.seventh').fadeTo("slow", 1, function(){})
     $('.loc').css("opacity", "1").addClass("right-list-anim")
     $(this).css("opacity", "0.8")
   }, function(){
-    $('.seventh').fadeTo("fast", 0, function(){})
     $('.loc').css("opacity", "0.5").removeClass("right-list-anim")
     $(this).css("opacity", "0.5")
   })
 
   $('#ecommerce').hover(function(){
-    $('.eighth').fadeTo("slow", 1, function(){})
     $('.ecom').css("opacity", "1").addClass("right-list-anim")
     $(this).css("opacity", "0.8")
   }, function(){
-    $('.eighth').fadeTo("fast", 0, function(){})
     $('.ecom').css("opacity", "0.5").removeClass("right-list-anim")
     $(this).css("opacity", "0.5")
   })
@@ -94,8 +77,16 @@ $(function(){
 
 
     $('.list-element').each(function(i, val){
-      $(this).hover(drawBars).hover(drawBars);
+      $(this).hover(drawBars, function(){$('.svg-bar').css("opacity", "0")})
+      // $(this).hover(
+      //   function(){
+      //     $('.svg-bar').fadeTo("slow", 1, function(){})
+      //   },function(){
+      //     $('.svg-bar').css("opacity", "0")
+      //   }
+      // );
       function drawBars(){
+        $('.svg-bar').css("opacity", "1")
         var allPoints = [];
         var rightIndexArray = $(this).data("connects-to").toString().split(",").map(function(num){return parseInt(num)});
         currentElement = i;
